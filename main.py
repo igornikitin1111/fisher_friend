ea51678a77d96785fb845a3ed7f06948810d847c
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-09-21 08:10:24 +0300

    pysimple+funkcija prideti_zuvi

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 import PySimpleGUI as sg
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import backend
+from backend import Base, Zuvis, Rusis, Vietove
 
-session = sessionmaker(backend.db_engine)()
+
+
+# Duomenu baze
+db_engine = create_engine('sqlite:///fisher_friend_database.db')
+
+# Sesijos kurimas
+Session = sessionmaker(db_engine)
+session = Session()
+
+# Funkcija zuvies pridejimui
+def prideti_zuvi(values):
+    svoris = float(values['-SVORIS-'])
+    ilgis = float(values['-ILGIS-'])
+    kada_pagauta = values['-KADA_PAGAUTA-']
+    rusis_pavadinimas = values['-RUSIS-']
+    vietoves_pavadinimas = values['-VIETOVE-']
+
+#Kitos funkcijos
+#
+#
+#
+
+# PySimpleGUI
+layout = [
+    [sg.TabGroup([
+    [sg.Tab('Prideti zuvi', [
+    [sg.Text("Svoris:"), sg.InputText(key='-SVORIS-')],
+    [sg.Text("Ilgis:"), sg.InputText(key='-ILGIS-')],
+    [sg.Text("Pagavimo data:"), sg.InputText(key='-KADA_PAGAUTA-')],
+    [sg.Text("Rusis:"), sg.InputText(key='-RUSIS-')],
+    [sg.Text("Vietove:"), sg.InputText(key='-VIETOVE-')],
+    [sg.Button("Prideti")]]),],
+    [sg.Tab('Istrinti zuvi', [
+    [sg.Text("Pasirinkite zuvi:"), sg.Listbox(values=[], size=(30, 10), key='-ZUVYS-')],
+    [sg.Button("Istrinti")]])],
+    [sg.Tab('Perziureti zuvis', [
+    [sg.Listbox(values=[], size=(30, 10), key='-ZUVYS-')]])]])]
+]
+
+window = sg.Window("Fisher Friend Programa", layout)
+window.close()
